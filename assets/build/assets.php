9ae4539f5ed42a2c8a3799ce8ec54feead2d2171
mahmudremal <mahmudremal@yahoo.com>
2024-05-15 12:24:31 +0600

    Updated Assets

--- assets/build/assets.php
+++ assets/build/assets.php
@@ -1,1 +1,1 @@
-<?php return array('js/admin.js' => array('dependencies' => array('wp-polyfill'), 'version' => '3607cddbaa69e2a2ff31e06ba408b5a0'), 'js/public.js' => array('dependencies' => array('wp-polyfill'), 'version' => '1f7e0981e46428adf6460413e02a76f7'));
+<?php return array('js/admin.js' => array('dependencies' => array('wp-polyfill'), 'version' => '572c5b52028758395208bc758790cf4c'), 'js/public.js' => array('dependencies' => array('wp-polyfill'), 'version' => '804d3ac3d865ca731a1df334af13e69e'));
